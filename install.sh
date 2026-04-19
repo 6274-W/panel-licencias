878c306336de0700839d6ac0a38a9e637deda9b1
6274-W <yojuego983@gmail.com>
2026-04-19 13:07:29 -0300

    install.sh

--- install.sh
+++ install.sh
@@ -1,36 +1,31 @@
 #!/bin/bash
 clear
-rm -rf /usr/local/bin/panel &> /dev/null
+rm -rf $0 &> /dev/null
 
-echo -e "\e[1;32m[+] Iniciando Instalador Elite...\e[0m"
+echo -e "\e[1;32m[+] Instalador RED APN PRO\e[0m"
 
-# Dependencias
-sudo apt-get update -y &> /dev/null
-sudo apt-get install -y libstdc++6 libcurl4 wget curl &> /dev/null
+apt update -y &> /dev/null
+apt install -y curl wget &> /dev/null
 
-echo -ne "\e[1;33m🔑 LLAVE DE ACTIVACIÓN: \e[0m"
+echo -ne "\e[1;33m🔑 INGRESE SU KEY: \e[0m"
 read KEY
-HWID=$(cat /etc/machine-id 2>/dev/null | cut -c1-10)
 
-SVR="prueba.red-pro.site"
+HWID=$(cat /etc/machine-id | cut -c1-10)
+
+SVR="TU-IP-DEL-VPS"  # ← CAMBIALO
+
 CHECK=$(curl -s "http://$SVR/licencias/validar.php?key=$KEY&hwid=$HWID")
 
 if [[ "$CHECK" == "OK" ]]; then
-    echo -e "\e[1;32m[+] Licencia Válida.\e[0m"
-    
-    # DESCARGA DIRECTA DEL BINARIO (Sin Base64)
-    wget -q --no-cache -O /usr/local/bin/panel "http://$SVR/core_engine"
-    
+    echo -e "\e[1;32m✔ Licencia válida\e[0m"
+
+    wget -q -O /usr/local/bin/panel "http://$SVR/licencias/download.php?key=$KEY"
+
     chmod +x /usr/local/bin/panel
-    
-    # Crear el alias
-    if ! grep -q "alias panel" ~/.bashrc; then
-        echo "alias panel='/usr/local/bin/panel'" >> ~/.bashrc
-    fi
-    
-    echo -e "\e[1;32m[+] INSTALACIÓN COMPLETADA.\e[0m"
-    echo -e "\e[1;37mCierre y abra su terminal o escriba: \e[1;33mpanel\e[0m"
+    echo "alias panel='/usr/local/bin/panel'" >> ~/.bashrc
+
+    echo -e "\e[1;32m✔ Instalado. Usa: panel\e[0m"
 else
-    echo -e "\e[1;31m[!] Error: $CHECK\e[0m"
+    echo -e "\e[1;31m✘ $CHECK\e[0m"
     exit 1
 fi
